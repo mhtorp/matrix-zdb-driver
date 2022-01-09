507c535884263127da9901e61e77e89c236a1120
mhtorp <mhtorp@gmail.com>
2022-01-09 23:59:18 +0100

    Normalize level commands to min/max range

--- matrix_zdb_dimmer.groovy
+++ matrix_zdb_dimmer.groovy
@@ -23,8 +23,8 @@ metadata {
         input name: "p2", type: "number", title: "2. Duration of dimming", range:"0..255", defaultValue: 5
         input name: "p3", type: "number", title: "3. Duration of on/off", range:"0..255", defaultValue: 0
         input name: "p4", type: "enum",   title: "4. Dimming mode", options:[[0:"No dimming"], [1:"Trailing Edge - TE"], [2:"Leading Edge - LE"]], defaultValue: 1
-        input name: "p5", type: "number", title: "5. Dimmer min level", range: "0..99", defaultValue: 0
-        input name: "p6", type: "number", title: "6. Dimmer max level", range: "1..99", defaultValue: 99
+        input name: "p5", type: "number", title: "5. Dimmer min level", range: "0..100", defaultValue: 0
+        input name: "p6", type: "number", title: "6. Dimmer max level", range: "1..100", defaultValue: 100
         input name: "p18", type: "number", title: "18. Dimmer on level (0 to remember last state)", range: "0..99", defaultValue: 0
         input name: "p26", type: "number", title: "26. Dimmer on level (0 to remember last state)", range: "0..99", defaultValue: 0
         input name: "p34", type: "number", title: "34. Dimmer on level (0 to remember last state)", range: "0..99", defaultValue: 0
@@ -37,8 +37,10 @@ def configure() {
     parent.setParameter(parameterNumber = 2, size = 1, value = p2.toInteger())
     parent.setParameter(parameterNumber = 3, size = 1, value = p3.toInteger())
     parent.setParameter(parameterNumber = 4, size = 1, value = p4.toInteger())
-    parent.setParameter(parameterNumber = 5, size = 1, value = p5.toInteger())
-    parent.setParameter(parameterNumber = 6, size = 1, value = p6.toInteger())
+    min_dim = min_dim > 99 ? 99 : min_dim
+    max_dim = max_dim > 99 ? 99 : max_dim
+    parent.setParameter(parameterNumber = 5, size = 1, value = min_dim.toInteger())
+    parent.setParameter(parameterNumber = 6, size = 1, value = max_dim.toInteger())
     if (!prestaging) {
         parent.setParameter(parameterNumber = 10 + prestage_button * 8, size = 4, value = get_dimmer_on_value(255))
     }
@@ -58,17 +60,25 @@ def get_dimmer_on_value(on_level) {
     )
 }
 
+def normalize_level(level) {
+    level = level > 100 ? 100 : level
+    level = level < 0 ? 0 : level
+    upper = p6
+    lower = p5
+    x = level / 100 * (upper - lower) + lower
+    return x as Integer
+}
+
 void setLevel(level, duration = 0) {
-    level = level < p5 ? p5 : level
     if (!prestaging || (device.currentValue("switch").toString() == "on")) {
         if (logEnable) log.debug "$device setLevel $level $duration"
-        parent.childSetLevel(device.deviceNetworkId, level as Integer, duration)
+        parent.childSetLevel(device.deviceNetworkId, normalize_level(level), duration)
     }
     def levelText = "${device.displayName} (${endpoint}) set to $level %"
     if (prestaging) {
         if (logEnable) log.debug "$device prestaging $level on $prestage_button"
         levelText = "${device.displayName} (${endpoint}) pre-staged to $level %"
-        parent.setParameter(parameterNumber = 10 + prestage_button * 8, size = 4, value = get_dimmer_on_value(level as Integer))
+        parent.setParameter(parameterNumber = 10 + prestage_button * 8, size = 4, value = get_dimmer_on_value(normalize_level(level)))
     }
     sendEvent(name: "level", value: level, descriptionText: levelText, type:"digital",unit:"%")
 }
